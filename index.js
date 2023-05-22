38cbb42fcb26e6a1148bae09e47a86d463cc7c52
Divyansh <divyanshgoel777@gmail.com>
2023-05-22 15:43:44 +0530

    Assignment

--- index.js
+++ index.js
@@ -7,19 +7,32 @@ const fs = require('fs');
 const file = argv[2]
 const value = argv[3]
 
-    fs.readFile(file,{encoding: 'utf8'},(err,data)=>{
-        if(err){
-            console.log("Something Wrong")
-        }else{
-            let arr = data.split(" ");
-            let count = 0
-            
-            for(let i=0;i<=arr.length-1;i++){
-                if(arr[i]==value){
+let count = 0
+fs.readFile(file, { encoding: 'utf8' }, (err, data) => {
+    if (err) {
+        console.log("Something Wrong")
+    } else {
+        let arr = data.split("\n");
+        console.log(arr)
+
+        for (let i = 0; i <= arr.length - 1; i++) {
+
+            let data1 = arr[i].split(" ")
+
+            for (let j = 0; j <= data1.length - 1; j++) {
+
+                if (data1[j] == value) {
                     count++
                 }
-            }
-            console.log(count)
-        }
-    })
 
+            }
+
+            // for(let i=0;i<=arr.length-1;i++){
+            //     if(arr[i]==value){
+            //         count++
+            //     }
+            // }
+        }
+    }
+    console.log(count)
+    })
